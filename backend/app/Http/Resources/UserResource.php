5f4e19882f01ef5944353f9e8007ef377e4529ce
Rafael <rafael271alves@gmail.com>
2022-07-17 18:25:27 -0300

    created user resource

---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+     /**
+     * The resource instance.
+     *
+     * @var \App\Models\User
+     */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $attrs = collect((array) parent::toArray($request));
+
+        if ($this->resource->getShowPrivatesAttribute()) {
+            $attrs->put('two_factor', $this->resource->hasEnabledTwoFactorAuthentication());
+        } else {
+            $attrs->forget([
+                'email'
+            ]);
+        }
+
+        return $attrs;
+    }
+}
