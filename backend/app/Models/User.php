da54872dcb29fd590a5978e6a48d982fcf25ee42
Rafael <rafael271alves@gmail.com>
2022-07-17 18:31:14 -0300

    added phpdoc in user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,18 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property string|null $two_factor_secret
+ * @property string|null $two_factor_recovery_codes
+ * @property string|null $two_factor_confirmed_at
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, TwoFactorAuthenticatable;
