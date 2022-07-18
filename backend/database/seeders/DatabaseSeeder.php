eb977e90138b013c93a03e4d8bbff5232816c9e3
Rafael <rafael271alves@gmail.com>
2022-07-18 20:54:39 -0300

    added user factory to seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory(20)->create();
     }
 }
