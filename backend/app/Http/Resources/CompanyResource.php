0f18fd3fe7409c1e844f76bf17e53e42d1efdc79
Rafael <rafael271alves@gmail.com>
2022-07-19 22:21:19 -0300

    created company resource

---
+++ backend/app/Http/Resources/CompanyResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * The resource instance.
+     *
+     * @var \App\Models\Company
+     */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $attrs = collect((array) parent::toArray($request));
+
+        if ($attrs->has('pivot')) {
+            $attrs->put('amount', $this->resource->pivot->amount);
+            $attrs->forget('pivot');
+        }
+
+        return $attrs;
+    }
+}
