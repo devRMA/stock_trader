c4706cdc06a1bd5c1f2fdff92aead30331e04bbf
Rafael <rafael271alves@gmail.com>
2022-07-19 21:59:55 -0300

    added more fields to user collection

--- backend/app/Http/Resources/UserCollection.php
+++ backend/app/Http/Resources/UserCollection.php
@@ -19,11 +19,15 @@ class UserCollection extends ResourceCollection
             'data' => parent::toArray($request),
             'first_page_url' => $this->url(1),
             'from' => $this->firstItem(),
+            'last_page' => $this->lastPage(),
+            'last_page_url' => $this->url($this->lastPage()),
+            'links' => $this->linkCollection()->toArray(),
             'next_page_url' => $this->nextPageUrl(),
             'path' => $this->path(),
             'per_page' => $this->perPage(),
             'prev_page_url' => $this->previousPageUrl(),
             'to' => $this->lastItem(),
+            'total' => $this->total(),
         ];
     }
 }
