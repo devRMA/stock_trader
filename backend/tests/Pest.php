c32de8cf07931b0c47d7aa8c5830d1572e41e735
Rafael <rafael271alves@gmail.com>
2022-07-20 20:00:12 -0300

    configured Pest

--- backend/tests/Pest.php
+++ backend/tests/Pest.php
@@ -1,3 +1,10 @@
 <?php
 
-uses(Tests\TestCase::class)->in('Feature');
+use function Pest\Laravel\seed;
+
+uses(Tests\TestCase::class)
+    ->in('Feature');
+
+uses(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->beforeEach(fn () => seed())
+    ->in('Feature');
