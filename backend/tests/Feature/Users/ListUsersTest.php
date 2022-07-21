e62d1071536981699fc49f96c739b2e208e9e5b0
Rafael <rafael271alves@gmail.com>
2022-07-20 21:37:03 -0300

    formating

--- backend/tests/Feature/Users/ListUsersTest.php
+++ backend/tests/Feature/Users/ListUsersTest.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Models\User;
-use function Pest\Laravel\getJson;
 use Illuminate\Testing\Fluent\AssertableJson;
+use function Pest\Laravel\getJson;
 
 it('should return the pagination with users order by money', function () {
     foreach (User::factory(20)->create() as $user) {
@@ -12,8 +12,7 @@ it('should return the pagination with users order by money', function () {
 
     getJson(route('users.index'))
         ->assertOk()
-        ->assertJson(fn(AssertableJson $json) =>
-            $json->where('total', 20)
+        ->assertJson(fn (AssertableJson $json) => $json->where('total', 20)
                 ->has('data', 10)
                 ->etc()
         );
