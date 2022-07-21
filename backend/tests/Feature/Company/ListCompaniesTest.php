82d2de30f13243ae1ac3d60eef845fecfe8195b3
Rafael <rafael271alves@gmail.com>
2022-07-20 22:20:54 -0300

    created tests for list company

---
+++ backend/tests/Feature/Company/ListCompaniesTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Company;
+use App\Models\User;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\getJson;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
+
+it('should return the list of all companies order by name', function () {
+    $companies = Company::all();
+
+    getJson(route('companies.index'))
+        ->assertOk()
+        ->assertJson(fn(AssertableJson $json) => $json->where('total', $companies->count())
+                ->has('data', $companies->count())
+                ->etc()
+        );
+});
+
+it('should return the list of all companies that users ', function () {
+    $companies = Company::factory(10)->create();
+    /** @var \App\Models\User */
+    $user = User::factory()->create();
+    $user->actions()->sync($companies);
+
+    Sanctum::actingAs($user, guard:'web');
+
+    getJson(route('companies.my_companies'))
+        ->assertJson(fn(AssertableJson $json) => $json->where('total', $companies->count())
+                ->has('data', $companies->count())
+                ->etc()
+        );
+
+    assertDatabaseCount('company_user', 10);
+});
