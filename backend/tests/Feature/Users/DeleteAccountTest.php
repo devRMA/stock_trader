8f6f7ef72d2026ac775a877976ab9ad038282479
Rafael <rafael271alves@gmail.com>
2022-07-20 21:48:03 -0300

    added more tests for delete user account

--- backend/tests/Feature/Users/DeleteAccountTest.php
+++ backend/tests/Feature/Users/DeleteAccountTest.php
@@ -20,3 +20,16 @@ it('should remove account from the database if user delete account', function ()
     ]);
     assertGuest('web');
 });
+
+it('should return error if try delete account without login', function () {
+    /** @var \App\Models\User */
+    $user = User::factory()->create();
+    /** @var \App\Models\User */
+    $other = User::factory()->create();
+
+    Sanctum::actingAs($user, guard:'web');
+
+    deleteJson(route('users.destroy', ['user' => $other]))
+        ->assertForbidden();
+});
+
