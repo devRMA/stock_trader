01a4c25ca4c04d7fef8ab1833fc43b018d16819e
Rafael <rafael271alves@gmail.com>
2022-07-30 17:51:13 -0300

    fix test

--- backend/tests/Feature/Company/BuyCompanyTest.php
+++ backend/tests/Feature/Company/BuyCompanyTest.php
@@ -26,7 +26,7 @@ it('should return error if the company has no more actions available', function 
     /** @var \App\Models\Company */
     $otherCompany = Company::factory()->create();
     $user->actions()->attach($otherCompany, [
-        'amount' => $company->max_actions - 1,
+        'amount' => $otherCompany->max_actions - 1,
     ]);
 
     postJson(route('companies.buy_actions', ['company' => $otherCompany]), [
