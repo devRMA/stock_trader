9f0a83368832fb8f78b5e0f40f2289647c796dd9
Rafael <rafael271alves@gmail.com>
2022-08-07 18:52:35 -0300

    added chakra to storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,15 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    stories: [
-        '../src/**/stories.mdx',
-        '../src/**/stories.@(js|jsx|ts|tsx)',
-    ],
+    stories: ['../src/**/stories.mdx', '../src/**/stories.@(js|jsx|ts|tsx)'],
     staticDirs: ['../public'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
+        '@chakra-ui/storybook-addon',
         {
             name: 'storybook-addon-next',
             options: {
@@ -17,6 +15,9 @@ module.exports = {
             },
         },
     ],
+    features: {
+        emotionAlias: false,
+    },
     framework: '@storybook/react',
     core: {
         builder: '@storybook/builder-webpack5',
