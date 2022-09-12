bce65283ac8d9226ea168ce1f2c36c3435670896
Rafael <rafael271alves@gmail.com>
2022-09-12 20:35:26 -0300

    added redux provider to app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,13 +3,17 @@ import '@fontsource/jetbrains-mono';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { Provider } from 'react-redux';
 
+import store from '../store';
 import theme from '../theme';
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
         </ChakraProvider>
     );
 }
