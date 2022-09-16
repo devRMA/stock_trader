1bdfdac354ce5bffd1b0352c48109408ed2333c9
Rafael <rafael271alves@gmail.com>
2022-09-15 22:23:52 -0300

    added initialColorMode

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 
+import theme from '../theme';
+
 export default class Document extends NextDocument {
     render() {
         return (
             <Html>
                 <Head />
                 <body>
-                    <ColorModeScript />
+                    <ColorModeScript
+                        initialColorMode={theme.config.initialColorMode}
+                    />
                     <Main />
                     <NextScript />
                 </body>
--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,10 +1,16 @@
-import { extendTheme } from '@chakra-ui/react';
+import { type ThemeConfig, extendTheme } from '@chakra-ui/react';
 
 const fonts = {
     body: `'JetBrains Mono', monospace`,
 };
 
+const config: ThemeConfig = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+};
+
 const theme = extendTheme({
+    config,
     fonts,
 });
 
