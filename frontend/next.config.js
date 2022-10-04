9f87fbafd92efd25b346fc294299cf393d7f2559
Rafael <rafael271alves@gmail.com>
2022-10-04 17:54:16 -0300

    disable pwa in dev mode

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 const { i18n } = require('./next-i18next.config');
 const withPWA = require('next-pwa')({
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
 });
 
 /** @type {import('next').NextConfig} */
