fa64d1a7c0bd1559f6b7905ca3084057aaf49bd3
Rafael <rafael271alves@gmail.com>
2022-10-04 13:11:52 -0300

    changed banned page

--- frontend/src/pages/banned.tsx
+++ frontend/src/pages/banned.tsx
@@ -1,72 +1,61 @@
 import {
     Box,
+    Container,
     Flex,
     Heading,
     Stack,
-    Table,
-    TableContainer,
-    Tbody,
-    Td,
     Text,
-    Textarea,
-    Th,
-    Thead,
-    Tr,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import illustrationData from 'assets/lotties/close-illustration.json';
+import DarkModeSwitch from 'components/DarkModeSwitch';
 import Header from 'components/Header';
 import Lottie from 'lottie-react';
 import { GetStaticPropsContext } from 'next';
-import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useEffectOnce } from 'react-use';
 import { selectUser, useAppSelector } from 'store/hooks';
 
 function Banned() {
     const { t } = useTranslation('banned');
-    const { logged, user } = useAppSelector(selectUser);
-    const router = useRouter();
-
-    useEffectOnce(() => {
-        if (!logged || !user.banned || user.bans.length === 0) {
-            router.push('/');
-        }
-    });
+    const { user } = useAppSelector(selectUser);
+    const mutedColor = useColorModeValue('gray.600', 'gray.500');
+    const borderColor = useColorModeValue('gray.200', 'gray.700');
 
     return (
         <>
             <Header />
-            <Stack align="center" p={20}>
+            <Stack align="center">
                 <Flex w="200px" h="200px">
-                    <Lottie animationData={illustrationData} loop autoPlay />
+                    <Lottie animationData={illustrationData} loop={false} />
                 </Flex>
-                <Heading as="h2" size="xl" mt={6} mb={2}>
-                    {t('title')}
-                </Heading>
-                <Text color="gray.500">{t('description')}</Text>
-                <TableContainer w="full" pt="5">
-                    <Table variant="simple">
-                        <Thead>
-                            <Tr>
-                                <Th>{t('reason')}</Th>
-                                <Th>{t('date')}</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {user.bans.map((ban) => (
-                                <Tr key={ban.created_at}>
-                                    <Td>{ban.reason}</Td>
-                                    <Td>
-                                        {new Date(
-                                            ban.created_at,
-                                        ).toLocaleString()}
-                                    </Td>
-                                </Tr>
-                            ))}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
+                <Heading size="xl">{t('title')}</Heading>
+                <Container>
+                    <Text color={mutedColor} mb={2} textAlign="justify">
+                        {t('description')}
+                    </Text>
+                    <Text as="span" color={mutedColor}>
+                        {t('date')}
+                    </Text>
+                    <Text as="span">
+                        {new Date(
+                            user.bans[user.bans.length - 1]?.created_at,
+                        ).toLocaleString()}
+                    </Text>
+                    <Box
+                        boxShadow="xl"
+                        border="1px"
+                        borderColor={borderColor}
+                        rounded="md"
+                        mt="2"
+                        p="2"
+                        textAlign="justify"
+                    >
+                        <Text color={mutedColor}>{t('reason')}</Text>
+                        <Text>{user.bans[user.bans.length - 1]?.reason}</Text>
+                    </Box>
+                </Container>
+                <DarkModeSwitch />
             </Stack>
         </>
     );
