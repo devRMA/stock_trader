ecd14bc473ef21cabe8af8fa4740f7f8279225cd
Rafael <rafael271alves@gmail.com>
2023-04-16 21:11:54 -0300

    created game config file

---
+++ backend/config/game.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'user' => [
+        // the amount of investocoins the user will start the game
+        'start_money' => 50_000,
+    ],
+    'company' => [
+        // the starting price to create a company
+        'price_to_create' => 10_000,
+        // the necessary level, for the company to enter the stock exchange
+        'level_to_ipo' => 5,
+    ],
+];
