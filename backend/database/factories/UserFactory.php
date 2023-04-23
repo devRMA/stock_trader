b9fd041bbc070fb04e01bbb896547434fc2b6386
Rafael <rafael271alves@gmail.com>
2023-04-23 10:16:36 -0300

    added states in user factory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -21,18 +22,39 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'money' => config('game.user.start_money'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the user has infinite money.
      */
-    public function unverified(): static
+    public function withInfinityMoney(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'money' => INF,
+        ]);
+    }
+
+    /**
+     * Indicate that the user has no money.
+     */
+    public function withoutMoney(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'money' => 0,
+        ]);
+    }
+
+    /**
+     * Indicate that the user has a specific amount of money.
+     */
+    public function withMoney(int $amount): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'money' => $amount,
         ]);
     }
 }
